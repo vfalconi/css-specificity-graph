1053f0a30652399a9ba5486ec52b18640f85a597
Vince Falconi <vince.falconi@gmail.com>
2014-11-10 11:54:16 -0600

    Updated `spec-calc.php`

--- spec-calc.php
+++ spec-calc.php
@@ -1,69 +1,69 @@
 <?php
 
-    function specificity($selector) {
+	function calculateSpecificity($selector)
+	{
+		$result ='';
+		$score = array(
+			'a' => 0,
+			'b' => 0,
+			'c' => 0
+		);
 
-        // Pseudo classes
-        $classes = array(":active", ":checked", ":default", ":dir()", ":disabled", ":empty", ":enabled", 
-            ":first", ":first-child", ":first-of-type", ":fullscreen", ":focus", ":hover", ":indeterminate", 
-            ":in-range", ":invalid", ":lang()", ":last-child", ":last-of-type", ":left", ":link", ":not()", 
-            ":nth-child()", ":nth-last-child()", ":nth-last-of-type()", ":nth-of-type()", ":only-child", 
-            ":only-of-type", ":optional", ":out-of-range", ":read-only", ":read-write", ":required", ":right", 
-            ":root", ":scope", ":target", ":valid", ":visited");
-        // Pseudo elements
-        $elements = array(":before", ":after", ":first-line", ":first-letter", ":selection");
-        // HTML tags
-        $tags = array("a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", 
-            "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", 
-            "content", "data", "datalist", "dd", "decorator", "del", "details", "dfn", "dialog", "div", "dl", 
-            "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", 
-            "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", 
-            "label", "legend", "li", "link", "main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav", 
-            "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", 
-            "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", 
-            "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", 
-            "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr");
+		$parts = array();
 
-        // Strip slashes
-        $data = preg_replace("/\/\\*[\\s\\S]*?\\*\//", "", $selector);      // Strip comments
-        $data = preg_replace("/\{(.*?)\}/s", ", ", $data);                  // Strip content and replace with delimiter
-        $data = str_replace(" ,", ",", $data);                              // Strip spaces after selectors
+		/*
+		*	This array follows the assumption of @keeganstreet's JS module, 'that selectors
+			matching the preceding regular expressions have been removed'.
+		*/
 
-        $tmp = explode(",", $data);                                         // Get rid of selector groupings
-        array_walk($tmp, 'trim');                                        // Trim whitespace from end of selectors
-        $score = 0;                                                         // Set the score to 0
-        // Loop through the selector array
-        foreach ($tmp as $var) {
-            if (!empty($var)) {
-                $score += substr_count($var, '#') * 100;                    // Count the IDs and multiple by 100 (i.e. #itemID)
-                $score += substr_count($var, '.') * 10;                     // Count the classes and multiple by 10 (i.e. .classname)
-                $score += substr_count($var, '[') * 10;                     // Count the attributes and multiple by 10 (i.e. input[type=text])
+		$selectorTypes = array(
+			'attribute' => array(
+				'pattern' => '/(\[[^\]]+\])/',
+				'type' => 'b'
+			),
+			'id' => array(
+				'pattern' => '/(#[^\s\+>~\.\[:]+)/',
+				'type' => 'a'
+			),
+			'class' => array(
+				'pattern' => '/(\.[^\s\+>~\.\[:]+)/',
+				'type' => 'b'
+			),
+			'pseudoElement' => array(
+				'pattern' => '/(::[^\s\+>~\.\[:]+|:first-line|:first-letter|:before|:after)/',
+				'type' => 'c'
+			),
+			// A regex for pseudo classes with brackets - :nth-child(), :nth-last-child(), :nth-of-type(), :nth-last-type(), :lang()
+			'pseudoClassWithBrackets' => array(
+				'pattern' => '/(:[\w-]+\([^\)]*\))/',
+				'type' => 'b'
+			),
+			// A regex for other pseudo classes, which don't have brackets
+			'pseudoClass' => array(
+				'pattern' => '/(:[^\s\+>~\.\[:]+)/',
+				'type' => 'b'
+			),
+			'element' => array(
+				'pattern' => '/([^\s\+>~\.\[:]+)/',
+				'type' => 'c'
+			),
+		);
 
-                foreach ($classes as $test) {                               // Count the pseudo-classes and multiple by 10
-                    $score += substr_count($var, $test) * 10;
-                }
+		// Remove `:not()` pseudo-class but leave the selector it negated
+		$selector = preg_replace('/:not\(([^\)]*)\)/', '$1', $selector);
 
-                foreach ($elements as $test) {                              // Count the pseudo-elements;
-                    $score += substr_count($var, $test);
-                }
+		$count = 0;
+		foreach ($selectorTypes as $type => $data)
+		{
+			$count = preg_match_all($data['pattern'], $selector, $matches);
 
-                $cleaned = str_replace(":link", " ", $var);                 // Remove the pseudo selectors that match HTML tags
+			for ($i = 0; $i < $count; $i++)
+			{
+				$score[$data['type']]++;
+				$selector = trim(preg_replace($data['pattern'], '', $selector, 1));
+			}
+		}
 
-                $cleaned = str_replace(":", " ", $cleaned);                 // Remove the pseudo selector
-                $cleaned = str_replace("+", " ", $cleaned);                 // Remove the adjacent sibling selector
-                $cleaned = str_replace(">", " ", $cleaned);                 // Remove the child selector
-                $cleaned = str_replace("*", " ", $cleaned);                 // Remove the universal selector
-                $cleaned = str_replace(".", " ", $cleaned);                 // Remove the class selector
-                $cleaned = str_replace("#", " ", $cleaned);                 // Remove the ID selector
-
-                $cleaned = explode(" ", $cleaned);                          // Explode on space
-                foreach ($cleaned as $rubbish) {                            // Count the type selectors (i.e. HTML tags)
-                    if (in_array(strtolower($rubbish), $tags)) {
-                        $score++;
-                    }
-                }
-            }
-        }
-
-        return $score;
-    }
+		return $score['a'] . $score['b'] . $score['c'];
+	}
 ?>
